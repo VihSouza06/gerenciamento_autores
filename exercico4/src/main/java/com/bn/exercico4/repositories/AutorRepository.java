60f63c62e15ad6710eaee447c0c38d528ff933d9
VihSouza06 <visouzasantos2006@gmail.com>
2026-03-15 18:24:52 -0300

    gerenciamento de autores

---
+++ exercico4/src/main/java/com/bn/exercico4/repositories/AutorRepository.java
@@ -0,0 +1,8 @@
+package com.bn.exercico4.repositories;
+
+import com.bn.exercico4.models.AutorModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AutorRepository extends JpaRepository<AutorModel, Long>{
+
+}
